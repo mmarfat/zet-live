04a86c9b8d6661ac322587eaa2df878e1b7409d6
mmarfat <markec_marf@hotmail.com>
2025-05-31 19:29:35 +0200

    feat: selected vehicle tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import Map from "./components/Map";
+import CustomSearchControl from "./components/CustomSearchControl";
+import FileDataProvider from "./contexts/FileDataContext/FileDataProvider";
 
-const App = () => {
+function App() {
+
+  const [selectedVehicleId, setSelectedVehicleId] = useState(null);
+
   return (
-    <Map />
-  )
+    <FileDataProvider>
+      <div className="relative w-full h-dvh">
+        <Map selectedVehicleId={selectedVehicleId} setSelectedVehicleId={setSelectedVehicleId} />
+        <div className="absolute top-0 left-0 w-full h-full pointer-events-none z-999">
+          <CustomSearchControl onSelect={setSelectedVehicleId} />
+        </div>
+      </div>
+    </FileDataProvider>
+  );
 }
 
 export default App;
--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -3,29 +3,31 @@ import { useState } from 'react';
 import { 
   TileLayer,
   MapContainer,
-  useMapEvents
+  useMapEvents,
+  AttributionControl
 } from 'react-leaflet';
 
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
 import { useTheme } from 'next-themes';
 import CustomZoomControl from '../CustomZoomControl';
-import CustomSearchControl from '../CustomSearchControl';
+import VehicleMarkers from './VehicleMarkers';
+import { useFileData } from '@/contexts/FileDataContext/FileDataContext';
+import FlyToVehicle from './FlyToVehicle';
 
-const Map = () => {
+const Map = ({ selectedVehicleId, setSelectedVehicleId }) => {
   
   const { resolvedTheme } = useTheme();
 
   const [center, setCenter] = useState([45.8, 15.985]);
   const [zoom, setZoom] = useState(13);
 
+  const { fileData } = useFileData();
+
   const MapEventTracker = () => {
     useMapEvents({
-      moveend: (e) => {
-        setCenter(e.target.getCenter());
-      },
-      zoomend: (e) => {
-        setZoom(e.target.getZoom());
-      },
+      moveend: (e) => setCenter(e.target.getCenter()),
+      zoomend: (e) => setZoom(e.target.getZoom()),
+      drag: () => setSelectedVehicleId(null)
     });
     return null;
   };
@@ -38,20 +40,22 @@ const Map = () => {
       scrollWheelZoom={true}
       zoomControl={false}
       className="h-dvh bg-background!"
+      attributionControl={false}
       whenCreated={(map) => {
         setCenter(map.getCenter());
         setZoom(map.getZoom());
       }}
     >
       <TileLayer
-        key={`${resolvedTheme}-tile-layer`}
         attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         className={resolvedTheme === "dark" ? "map-tiles-dark" : null}
       />
       <MapEventTracker />
-      <CustomSearchControl />
       <CustomZoomControl />
+      {fileData && <VehicleMarkers selectedVehicleId={selectedVehicleId} setSelectedVehicleId={setSelectedVehicleId} />}
+      {fileData && <FlyToVehicle selectedVehicleId={selectedVehicleId} />}
+      <AttributionControl position="bottomright" prefix={'&copy; <a href="https://www.zet.hr/gtfs-rt-protobuf">ZET GTFS-RT</a>'} />
     </MapContainer>
   );
 };
